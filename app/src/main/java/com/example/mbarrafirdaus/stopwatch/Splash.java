a1283a60e4ca413497e217ebc70ba77d40581473
muhF1508 <firdausbarra08@gmail.com>
2018-11-06 16:48:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mbarrafirdaus/stopwatch/Splash.java
@@ -0,0 +1,29 @@
+package com.example.mbarrafirdaus.stopwatch;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000L);
+    }
+}
